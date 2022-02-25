57007a3673230a615c786ef91bed9fafbd57ec63
Nsisong Akpan <sandersakpan@gmail.com>
2022-02-25 15:54:56 +0100

    implemented meeting room ui

--- src/components/chat/sidepane.vue
+++ src/components/chat/sidepane.vue
@@ -1,7 +1,104 @@
 <template>
-<div>
+  <div>
     <div class="side-pane">
-        <h4>Side Pane</h4>
+      <div class="chat_wrap">
+        <div
+          class="
+            chat_hearder_wrap
+            d-flex
+            align-items-center
+            justify-content-between
+          "
+        >
+          <div class="room_wrap">
+            <h6 class="font-weight-bold text-white">ROOMS</h6>
+          </div>
+          <div class="span_wrap mt-1">
+            <ion-icon class="text-white" md="md-add-circle"></ion-icon>
+          </div>
+        </div>
+        <div class="chat_list_wrap mt-3">
+          <ul>
+            <li>
+              <img src="@/assets/male_avatar.png" alt="" />
+              <div class="write_up_wrap">
+                <p>Ibom Policies</p>
+                <span>Discuss state policies</span>
+              </div>
+            </li>
+            <li>
+              <img src="@/assets/male_avatar.png" alt="" />
+              <div class="write_up_wrap">
+                <p>Ibom Policies</p>
+                <span>Discuss state policies</span>
+              </div>
+            </li>
+            <li>
+              <img src="@/assets/male_avatar.png" alt="" />
+              <div class="write_up_wrap">
+                <p>Ibom Policies</p>
+                <span>Discuss state policies</span>
+              </div>
+            </li>
+            <li>
+              <img src="@/assets/male_avatar.png" alt="" />
+              <div class="write_up_wrap">
+                <p>Ibom Policies</p>
+                <span>Discuss state policies</span>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <!-- Direct Chat Section -->
+        <div
+          class="
+            chat_hearder_wrap
+            d-flex
+            align-items-center
+            justify-content-between
+            mt-5
+          "
+        >
+          <div class="room_wrap">
+            <h6 class="font-weight-bold text-white">DIRECT CHAT</h6>
+          </div>
+          <div class="span_wrap mt-1">
+            <ion-icon class="text-white" md="md-add-circle"></ion-icon>
+          </div>
+        </div>
+        <div class="chat_list_wrap mt-3">
+          <ul>
+            <li>
+              <img src="@/assets/male_avatar.png" alt="" />
+              <div class="write_up_wrap">
+                <p>Ibom Policies</p>
+                <span>Finance Manager</span>
+              </div>
+            </li>
+            <li>
+              <img src="@/assets/male_avatar.png" alt="" />
+              <div class="write_up_wrap">
+                <p>Emmanuel Dynasty</p>
+                <span>Finance Manager</span>
+              </div>
+            </li>
+            <li>
+              <img src="@/assets/male_avatar.png" alt="" />
+              <div class="write_up_wrap">
+                <p>Emmanuel Dynasty</p>
+                <span>Finance Manager</span>
+              </div>
+            </li>
+            <li>
+              <img src="@/assets/male_avatar.png" alt="" />
+              <div class="write_up_wrap">
+                <p>Emmanuel Dynasty</p>
+                <span>Finance Manager</span>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-</div>
+  </div>
 </template>
